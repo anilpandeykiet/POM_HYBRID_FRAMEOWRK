62ec03fae898326a8f07636670606099d538f2ad
Anil Pandey <anil.kumar.pandey@jci.com>
2017-06-14 13:06:10 +0530

    File Reading package complete

--- CoreModule/fileReadingPkg/ReadTextFile.java
+++ CoreModule/fileReadingPkg/ReadTextFile.java
@@ -3,10 +3,68 @@
  */
 package fileReadingPkg;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
- * @author cpandeak
+ * @author Anil Pandey
  *
  */
 public class ReadTextFile {
+
+	private static ReadTextFile INSTANCE = null;
+	private static StringBuffer stringbuffer = new StringBuffer();
+	private static String fileContent = null;
+	private static BufferedReader bufferedReader = null;
+	private static FileReader fileReader = null;
+
+	private ReadTextFile(String textFilePath) {
+		try {
+			fileReader = new FileReader(textFilePath);
+			bufferedReader = new BufferedReader(fileReader);
+
+		} catch (Exception e) {
+			// e.printStackTrace();
+			System.out.println("Error reading the text file: " + textFilePath + "\n" + e.getMessage());
+		}
+	}
+
+	public static ReadTextFile getInstance(String textFilePath) {
+		if (INSTANCE == null) {
+			INSTANCE = new ReadTextFile(textFilePath);
+		}
+		return INSTANCE;
+	}
+
+	/**
+	 * Gets the text file content.
+	 *
+	 * @return the text file content
+	 */
+	public StringBuffer getTextFileContent() {
+		if (bufferedReader != null && fileReader != null) {
+			try {
+				while ((fileContent = bufferedReader.readLine()) != null) {
+					stringbuffer.append(fileContent);
+					stringbuffer.append(System.lineSeparator());
+				}
+			} catch (IOException e) {
+				System.out.println("Error reading the text file" + e.getMessage());
+			} finally {
+				if (bufferedReader != null && fileReader != null) {
+					try {
+						bufferedReader.close();
+						fileReader.close();
+					} catch (IOException e) {
+						System.out.println("Error closing 'bufferedReader' or 'fileReader'" + e.getMessage());
+					}
+				}
+			}
+		} else {
+			System.out.println("all the readers are null....");
+		}
+		return stringbuffer;
+	}
 
 }
--- TestModule/frameworkTestMethods/test1.java
+++ TestModule/frameworkTestMethods/test1.java
@@ -1,5 +1,15 @@
 package frameworkTestMethods;
-import java.net.URL;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.internet.MimeBodyPart;
+
+import emailPkg.EmailSender;
+import fileReadingPkg.ReadPropertyFile;
+import fileReadingPkg.ReadTextFile;
 
 /**
  * 
@@ -15,7 +25,27 @@ public class test1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(System.getProperty("user.dir"));
+		ReadPropertyFile rpr = ReadPropertyFile.getInstance("./TestResources/TestConfig/test.properties");
+		
+		EmailSender.sendEmail("true", rpr.getKey("emailConfigFile"));
+/*		System.out.println(System.getProperty("user.dir"));
+		
+		Set<String> props = rpr.getAllProperties();
+		
+		System.out.println(rpr.getKey("emailBodyTextFile"));
+
+		Iterator<String> iterator = props.iterator();
+		
+		
+		ReadTextFile rt = ReadTextFile.getInstance("./TestResources/TestConfig/EmailBodyContent.txt");*/
+		
+	//	System.out.println(rt.getTextFileContent());
+		
+		
+/*		while(iterator.hasNext()){
+			String text= iterator.next();
+			System.out.println(text+"="+rpr.getKey(text));
+		}*/
 	}
 
 }
