0e01a27a62c087c361f9429a4b8210e9a6c30843
Rajarshi Tripathi <merajarshi2@gmail.com>
2017-06-19 17:01:37 +0530

    1. WebRadiobutton.java (Modify)

--- WebModule/com/webElementPkg/WebRadiobutton.java
+++ WebModule/com/webElementPkg/WebRadiobutton.java
@@ -8,5 +8,22 @@ package com.webElementPkg;
  *
  */
 public class WebRadiobutton {
+//Get value of radio button from user
+	
+	public static void radioButtonOption(WebDriver driver, ExtentTest logger, WebElement element, String radioOption){
+		try {
+			if (element != null) {
+				element.sendKeys(radioOption);
+				logger.log(LogStatus.PASS, "Option <label>'"+radioOption +"'</label> saved successfully in <label>'"+element.getAttribute("RadioButtonOption")+"'</label>");
+			}
+		} catch (Exception e) {
+			logger.log(LogStatus.FAIL, "Failed to find <label>'"+element.getAttribute("RadioButtonOption")+"'</label>");
+		}
+}
 
+	//FInd the radio button corresponding the input
+
+	driver.findElements(By.cssSelector("input[value = "+ radioOption +" ]).clcik () ;
+	
+	
 }
